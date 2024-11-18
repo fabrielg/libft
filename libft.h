17fa6521703661a43bb88bfa38ca51ad88f150cc
Gabriel Francois <gfrancois2003@gmail.com>
2024-11-18 16:28:13 +0100

    feat: bonus| add ft_lstadd_front function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:41:14 by gfrancoi          #+#    #+#             */
-/*   Updated: 2024/11/18 16:13:19 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2024/11/18 16:27:46 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,6 @@ typedef struct s_list
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
